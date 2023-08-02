77b8e7a0fed13a222c559a6399c12b633fa842cd
UthpalaP <UthpalaP@99x.io>
2023-08-02 14:15:29 +0530

    #9 search bar ui completed

--- client/src/components/Search/SearchInput.jsx
+++ client/src/components/Search/SearchInput.jsx
@@ -1,41 +1,34 @@
-import React from 'react';
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import React from "react";
+import {
+  Button,
+  Col,
+  // Container,
+  Form,
+  Row,
+} from "react-bootstrap";
 
 const SearchInput = () => {
+ 
+
   return (
-    <Container className="mt-5">
-    <Row>
-      <Col lg={7}>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="primary">
-            Search
-          </Button>
-          
-        </Form>
-       
-      </Col>
-    </Row>
-    <Col lg={7}>
+    // <Container className="mt-5">
+ 
+      <Row>
+        <Col >
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="primary">Search</Button>
+          </Form>
+        </Col>
+      </Row>
+    
 
-
-    <Row>
-          <Button variant="primary">
-            Name
-          </Button>
-          <Button variant="primary">
-            Designation
-          </Button>
-
-
-          </Row>
-    </Col>
-  </Container>
+    // </Container>
   );
 };
 
--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import SearchInput from '../components/Search/SearchInput';
-import SearchResultList from '../components/Search/SearchResultList ';
-import {Container, Col, Row} from 'react-bootstrap';
+import React, { useState } from "react";
+import SearchInput from "../components/Search/SearchInput";
+import SearchResultList from "../components/Search/SearchResultList ";
+import { Container, Col, Row, ButtonToolbar, Button } from "react-bootstrap";
 // import {Row} from 'react-bootstrap'
 
-
 const SearchPage = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [activeButton, setActiveButton] = useState("button1"); // Make the first button active initially
 
   const handleSearch = async () => {
-    if (searchQuery.trim() === '') {
+    if (searchQuery.trim() === "") {
       return;
     }
 
@@ -22,31 +22,82 @@ const SearchPage = () => {
 
     // For this example, let's assume the results are fetched after a short delay
     setTimeout(() => {
-      const results = ['Result 1', 'Result 2', 'Result 3']; // Replace with actual search results
+      const results = ["Result 1", "Result 2", "Result 3"]; // Replace with actual search results
       setSearchResults(results);
       setIsLoading(false);
     }, 1000);
   };
 
+  const buttons = [
+    { value: "button1", text: "Name" },
+    { value: "button2", text: "Specialization" },
+    { value: "button3", text: "Designation" },
+    { value: "button4", text: "Current Workplace" },
+    { value: "button5", text: "Institute" },
+  ];
+
+  const handleButtonClick = (buttonNumber) => {
+    setActiveButton(buttonNumber);
+  };
+
   return (
-    <Container>
+    <Container className="mt-5">
       <Row></Row>
-      <Row >
+      <Row>
         <Col></Col>
-        <Col><h4>Slasscom Employee Search</h4></Col>
+        <Col>
+          <h4>Slasscom Employee Search</h4>
+        </Col>
         <Col></Col>
       </Row>
       <Row>
-        <Col sm={4}></Col>
-        <Col sm={8}>  <SearchInput
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onSearch={handleSearch}
-        />
-        {isLoading ? <p>Loading...</p> : <SearchResultList results={searchResults} />}</Col>
+        <Col md={2}> <Container className="mt-5"><h4>Search By</h4></Container></Col>
+        <Col md={10}>
+          <Container className="mt-5">
+            <Col sm={2}>
+              
+            </Col>
+            <Col sm={8}>
+              <ButtonToolbar className="justify-content-between">
+                {buttons.map((button) => (
+                  <Button
+                    key={button.value}
+                    variant={
+                      activeButton === button.value ? "primary" : "secondary"
+                    }
+                    onClick={() => handleButtonClick(button.value)}
+                  >
+                    {button.text}
+                  </Button>
+                ))}
+              </ButtonToolbar>{" "}
+            </Col>
+          </Container>
+        </Col>
       </Row>
-    </Container>)
-       
+      <Row>
+        <Col md={2}></Col>
+        <Col md={10}>
+          <Container className="mt-4">
+            <Col sm={3}></Col>
+            <Col sm={8}>
+              {" "}
+              <SearchInput
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                onSearch={handleSearch}
+              />
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                <SearchResultList results={searchResults} />
+              )}
+            </Col>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default SearchPage;
