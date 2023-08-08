29b196c9f94876ea6dc95eb8ab67492dc2f8b64d
UthpalaP <UthpalaP@99x.io>
2023-08-08 13:22:50 +0530

    #17 commented codes remved

--- client/src/utils/getAwsConfig.ts
+++ client/src/utils/getAwsConfig.ts
@@ -1,4 +1,3 @@
-// import config from "./config.json";
 
 export function getAwsConfig() {
   return {
@@ -19,14 +18,6 @@ export function getAwsConfig() {
       },
     },
     API: {
-      // endpoints: [
-      //     {
-      //         name: 'MyFunction',
-      //         endpoint: 'https://lambda.eu-west-1.amazonaws.com/2015-03-31/functions/MyFunction/invocations',
-      //         service: 'lambda',
-      //         region: 'eu-east-1',
-      //     },
-      // ],
       aws_appsync_graphqlEndpoint:process.env.REACT_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
       aws_appsync_region: process.env.REACT_APP_AWS_APPSYNC_REGION,
       aws_appsync_authenticationType: process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATION_TYPE,
